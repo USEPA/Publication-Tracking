1b6cd42ca2d9b426aa79d07c416a38814468ec96
Casey Kriutzfield <casey.kriutzfield@microsoft.com>
2022-10-28 11:06:38 -0400

    Adding the project and configuring the files.

---
+++ Pages/Database.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PublicationTracking.Pages;
+
+public class DatabaseModel : PageModel
+{
+    private readonly ILogger<DatabaseModel> _logger;
+
+    public DatabaseModel(ILogger<DatabaseModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
