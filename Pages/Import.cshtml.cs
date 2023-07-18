f39e0744d7b0e1d27cf4ff5eddb7ebb0f4853a3f
Casey Kriutzfield <casey.kriutzfield@live.com>
2023-07-18 12:05:54 -0400

    Updated import items, added Template.

--- Pages/Import.cshtml.cs
+++ Pages/Import.cshtml.cs
@@ -34,9 +34,7 @@ public class ImportModel : PageModel
             var importFile = form.Files[0];
             var extension = Path.GetExtension(importFile.FileName);
             if(extension == ".xlsx")
-            {
-                Console.WriteLine("We have an excel file!");
-                
+            {                
                 // From here we read the excel file, and use that to create the publications.
                 using (var stream = importFile.OpenReadStream())
                 {
@@ -51,34 +49,36 @@ public class ImportModel : PageModel
                                 continue;
                             }
                             // Then we go though each column and use it.
+                            // Note, right now, we are only using DocumentId, ResponsibleCode, AlphaDescriptor, and Title.
+                            // If we want to include new columns, just need to make sure they are in the excel file, and then uncomment the code below.
                             var p = new Data.Publication();
                             p.DocumentId = row.ItemArray[0].ToString();
 
                             // Need to make sure that the Responsible Code is valid.
                             p.ResponsibleCode = _context.ResponsibleCodes.Where(r => r.Code == row.ItemArray[1].ToString()).FirstOrDefault();
                             p.AlphaDescriptor = _context.AlphaDescriptors.Where(a => a.Code == row.ItemArray[2].ToString()).FirstOrDefault();
-                            p.Year = int.Parse(row.ItemArray[3].ToString());
-                            p.SequenceNumber = int.Parse(row.ItemArray[4].ToString());
-                            p.Volume = row.ItemArray[5]?.ToString();
-                            p.Title = row.ItemArray[6].ToString();
-                            p.Comment = row.ItemArray[7]?.ToString();
-                            p.Url = row.ItemArray[8]?.ToString();
-                            p.Language = Enum.TryParse(row.ItemArray[9].ToString(), out Data.Language language) ? language : Data.Language.English;
-                            p.DateRequested = DateTime.Parse(row.ItemArray[10].ToString());
-                            p.PointOfContactName = row.ItemArray[11].ToString();
-                            p.PointOfContactOrganization = row.ItemArray[12]?.ToString();
-                            p.PointOfContactPhoneNumber = row.ItemArray[13]?.ToString();
-                            p.PointOfContactEmail = row.ItemArray[14]?.ToString();
-                            p.PointOfContactMailCode = row.ItemArray[15]?.ToString();
-                            p.IsOriginal = (bool?)row.ItemArray[16];
-                            p.IsDigital = (bool?)row.ItemArray[17];
-                            p.IsPrinted = (bool?)row.ItemArray[18];
-                            p.IsInternalOnly = (bool?)row.ItemArray[19];
-                            p.OriginalId = row.ItemArray[20]?.ToString();
-                            p.Revision = row.ItemArray[21]?.ToString();
-                            p.ExpectedPublicationDate = DateTime.Parse(row.ItemArray[22].ToString());
-                            p.DateEntered = DateTime.Parse(row.ItemArray[22].ToString());
-                            p.EnteredBy = row.ItemArray[24].ToString();
+                            //p.Year = int.Parse(row.ItemArray[3].ToString());
+                            //p.SequenceNumber = int.Parse(row.ItemArray[4].ToString());
+                            //p.Volume = row.ItemArray[5]?.ToString();
+                            p.Title = row.ItemArray[3].ToString();
+                            //p.Comment = row.ItemArray[7]?.ToString();
+                            //p.Url = row.ItemArray[8]?.ToString();
+                            //p.Language = Enum.TryParse(row.ItemArray[9].ToString(), out Data.Language language) ? language : Data.Language.English;
+                            //p.DateRequested = DateTime.Parse(row.ItemArray[10].ToString());
+                            //p.PointOfContactName = row.ItemArray[11].ToString();
+                            //p.PointOfContactOrganization = row.ItemArray[12]?.ToString();
+                            //p.PointOfContactPhoneNumber = row.ItemArray[13]?.ToString();
+                            //p.PointOfContactEmail = row.ItemArray[14]?.ToString();
+                            //p.PointOfContactMailCode = row.ItemArray[15]?.ToString();
+                            //p.IsOriginal = (bool?)row.ItemArray[16];
+                            //p.IsDigital = (bool?)row.ItemArray[17];
+                            //p.IsPrinted = (bool?)row.ItemArray[18];
+                            //p.IsInternalOnly = (bool?)row.ItemArray[19];
+                            //p.OriginalId = row.ItemArray[20]?.ToString();
+                            //p.Revision = row.ItemArray[21]?.ToString();
+                            //p.ExpectedPublicationDate = DateTime.Parse(row.ItemArray[22].ToString());
+                            //p.DateEntered = DateTime.Parse(row.ItemArray[22].ToString());
+                            //p.EnteredBy = row.ItemArray[24].ToString();
 
                             publications.Add(p);
                             importResults.Add(new Data.ImportResults { DocumentId = p.DocumentId, IsSuccess = true });
@@ -100,5 +100,11 @@ public class ImportModel : PageModel
     public ActionResult CreateResult([FromBody] List<Data.ImportResults> importResults)
     {
         return Page();
+    }
+
+    public ActionResult DownloadFile()
+    {
+        Console.WriteLine("DownloadFile");
+        return File("/files/TestFile34.csv", "application/octet-stream", "Template.xlsx");
     }
 }
